439ea7a0937dec7692dcc9e3a7cc8124ef802593
Rohit10jr <rohitjworkspace@gmail.com>
2024-05-24 20:44:18 +0530

    final changes complete

--- app.py
+++ app.py
@@ -10,27 +10,24 @@ connection_string = "mysql+mysqlconnector://root:1234@localhost:3306/ems_db_11"
 engine = create_engine(connection_string)
 metadata = MetaData()
 
-# Reflect the table
 meter_data_100 = Table('meter_data_100', metadata, autoload_with=engine)
-
 
 @app.route('/')
 def index():
     # Query columns using pandas
     df = pd.read_sql("SELECT tt, V1, V2, V3, V4, V12 FROM meter_data_100 LIMIT 1000", engine)
     # df = pd.read_sql("SELECT tt, V1, V2, V3, V4, V12 FROM meter_data_100", engine)
-    # Convert DataFrame to JSON format
     data_list = df.to_dict(orient='records')
     # print(data_list)
-    return render_template('plot5.html', data=data_list)
+    return render_template('plots.html', data=data_list)
 
 
-@app.route('/one')
+@app.route('/single-plot')
 def one():
     # Query the necessary columns using pandas
     df = pd.read_sql("SELECT tt, V1, V2, V3, V4, V12 FROM meter_data_100 LIMIT 1000", engine)
     data_list = df.to_dict(orient='records')
-    return render_template('oneplot.html', data=data_list)
+    return render_template('plot.html', data=data_list)
 
 @app.route('/list')
 def list():
@@ -46,11 +43,7 @@ def list():
             meter_data_100.c.V12
         )
         result = connection.execute(query)
-        print('---------')
-        print(type(result))
-        # Fetch all rows
         rows = result.fetchall()
-        print(type(rows))
         # Convert to a list of dictionaries
         data_list = [dict(row._mapping) for row in rows]
 
